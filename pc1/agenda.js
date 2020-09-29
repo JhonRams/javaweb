e41bd69437a17ba45f646895d6ce339b95470ae2
JhonRams <64045380+JhonRams@users.noreply.github.com>
2020-09-29 16:51:57 -0500

    Add files via upload

---
+++ pc1/agenda.js
@@ -0,0 +1,8 @@
+//var tarea=document.getElementsByClassName('tarea');
+//for (var i = 0; i <tarea.length; i++) {
+//    tarea[i].addEventListener("click",ver_tarea);
+//}
+//function ver_tarea(){
+//    
+//}
+
